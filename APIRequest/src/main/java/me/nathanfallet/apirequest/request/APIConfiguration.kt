fca893e53d47bc8cd33d905f99fac89d4cd5caaa
Nathan Fallet <contact@nathanfallet.me>
2022-03-03 12:28:35 +0100

    Working, need to document and publish now

---
+++ APIRequest/src/main/java/me/nathanfallet/apirequest/request/APIConfiguration.kt
@@ -0,0 +1,19 @@
+package me.nathanfallet.apirequest.request
+
+import me.nathanfallet.apirequest.decoder.APIDecoder
+import me.nathanfallet.apirequest.decoder.JSONAPIDecoder
+import me.nathanfallet.apirequest.encoder.APIEncoder
+import me.nathanfallet.apirequest.encoder.JSONAPIEncoder
+
+class APIConfiguration(
+    val host: String,
+    val scheme: String = "https",
+    val port: Int? = null,
+) {
+
+    // Configuration variables
+    var headers: () -> HashMap<String, String> = { HashMap() }
+    var encoder: APIEncoder = JSONAPIEncoder()
+    var decoder: APIDecoder = JSONAPIDecoder()
+
+}
--- app/src/main/java/me/nathanfallet/apirequestsample/MainActivity.kt
+++ app/src/main/java/me/nathanfallet/apirequestsample/MainActivity.kt
@@ -2,10 +2,41 @@ package me.nathanfallet.apirequestsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import me.nathanfallet.apirequest.decoder.JSONAPIDecoder
+import me.nathanfallet.apirequest.encoder.JSONAPIEncoder
+import me.nathanfallet.apirequest.request.APIConfiguration
+import me.nathanfallet.apirequest.request.APIRequest
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var statusText: TextView
+    private lateinit var mainText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        statusText = findViewById(R.id.statusText)
+        mainText = findViewById(R.id.mainText)
+
+        // Create configuration
+        val config = APIConfiguration("jsonplaceholder.typicode.com").also {
+            // Optional personalization
+            it.headers = {
+                // Add custom headers (eg: an access token)
+                HashMap()
+            }
+            it.encoder = JSONAPIEncoder() // Set a custom APIEncoder
+            it.decoder = JSONAPIDecoder() // Set a custom APIDecoder
+        }
+
+        // Create a request
+        APIRequest("GET", "/comments", config)
+            .with("postId", 1)
+            .execute { result, status ->
+                mainText.text = result.toString()
+                statusText.text = status.toString()
+            }
     }
+
 }
